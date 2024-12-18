9c703ccd87ec33e0dc6358dfa94e35e121ac0c02
LaurGen <oprealaur@yahoo.com>
2024-12-18 14:38:41 +0200

    causeeffect + boundary

--- src/main/java/com/tss/Main.java
+++ src/main/java/com/tss/Main.java
@@ -2,13 +2,5 @@ package com.tss;
 
 public class Main {
     public static void main(String[] args) {
-        var demoObject = new Demo();
-        System.out.println(demoObject.method("abcdef"));
-        System.out.println(demoObject.method("Abcdef"));
-        System.out.println(demoObject.method("ABCDEF"));
-        System.out.println(demoObject.method("abcdef1"));
-        System.out.println(demoObject.method("abcdef@"));
-        System.out.println(demoObject.method("abc"));
-        System.out.println(demoObject.method("abcdefabcdefabcdefabcdef"));
     }
 }
--- src/test/java/cause_effect_graphing.java
+++ src/test/java/cause_effect_graphing.java
@@ -1,2 +1,64 @@
+import Helpers.CaptureConsole;
+import Helpers.RandomString;
+import com.tss.Demo;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class cause_effect_graphing {
+
+    private final Demo demo = new Demo();
+    private final RandomString randomString = new RandomString();
+
+    @Rule
+    public final CaptureConsole captureConsoleRule = new CaptureConsole();
+
+    @Test
+    public void TC1_under5Char() {
+        assertFalse(demo.method(""));
+        assertFalse(demo.method("a"));
+        assertFalse(demo.method("ab"));
+        assertFalse(demo.method("abc"));
+        assertFalse(demo.method("abcd"));
+    }
+
+    @Test
+    public void TC2_5to20Char_mixed(){
+        for (int i = 5; i < 21; i++) {
+            assert demo.method(randomString.generate(i));
+        }
+    }
+
+    @Test
+    public void TC3_over20Char(){
+        assertFalse(demo.method(randomString.generate(21)));
+    }
+
+    @Test
+    public void TC4_5to20Char_lowercase(){
+        for (int i = 5; i < 21; i++) {
+            assert demo.method(randomString.generateLower(i));
+            assert captureConsoleRule.getOutput().contains("Lowercase");
+        }
+    }
+
+    @Test
+    public void TC5_5to20Char_uppercase(){
+        for (int i = 5; i < 21; i++) {
+            assert demo.method(randomString.generateUpper(i));
+            assert captureConsoleRule.getOutput().contains("Uppercase");
+        }
+    }
+
+    @Test
+    public void TC6_5to20Char_Number_or_Symbol(){
+        for (int i = 4; i < 20; i++) {
+            assertFalse(demo.method(randomString.generate(i)+"1"));
+        }
+
+        for (int i = 4; i < 20; i++) {
+            assertFalse(demo.method(randomString.generate(i) + "!"));
+        }
+    }
 }
